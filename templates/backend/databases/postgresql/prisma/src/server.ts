ed1959db2271240afbf93f991de8350b7b8924e2
hieudoanm <hieumdoan@gmail.com>
2025-02-07 16:50:27 +0700

    fix build

--- templates/backend/databases/postgresql/prisma/src/server.ts
+++ templates/backend/databases/postgresql/prisma/src/server.ts
@@ -1,15 +1,54 @@
 import { PrismaClient } from '@prisma/client';
-import { createServer } from 'node:http';
+import { createServer, IncomingMessage, ServerResponse } from 'node:http';
 
 const prismaClient = new PrismaClient();
 
-const server = createServer(async (req, res) => {
-  await prismaClient.$connect();
-  const users = await prismaClient.user.findMany();
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ users }));
-  await prismaClient.$disconnect();
-});
+const server = createServer(
+  async (
+    request: IncomingMessage,
+    response: ServerResponse<IncomingMessage> & {
+      req: IncomingMessage;
+    },
+  ) => {
+    try {
+      if (request.url === 'users') {
+        if (request.method === 'GET') {
+          await prismaClient.$connect();
+          const users = await prismaClient.user.findMany();
+          await prismaClient.$disconnect();
+          response.writeHead(200, { 'Content-Type': 'application/json' });
+          response.end(JSON.stringify({ error: null, users }));
+          return;
+        } else if (request.method === 'POST') {
+          let body = '';
+          request.on('data', (chunk) => (body += chunk));
+          request.on('end', async () => {
+            const { username = '', email = '' } = JSON.parse(body);
+            const user = await prismaClient.user.create({
+              data: { username, email },
+            });
+            await prismaClient.$disconnect();
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify({ error: null, user }));
+          });
+          return;
+        } else {
+          response.writeHead(405, { 'Content-Type': 'application/json' });
+          response.end(JSON.stringify({ error: 'Method Not Allowed' }));
+        }
+      } else {
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({ error: 'Not Found' }));
+      }
+    } catch (error) {
+      console.error(error);
+      response.writeHead(500, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ error: 'Internal Server Error' }));
+    } finally {
+      await prismaClient.$disconnect();
+    }
+  },
+);
 
 // starts a simple http server locally on port 8080
 server.listen(8080, '127.0.0.1', () => {
